9ef9410d07a03487e7e821140e6122b5f718c4c6
RaimondoCosma <raimondo.cosma@gmail.com>
2023-01-13 17:57:11 +0100

    Aggiunge header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+    <section class="bg-pre-header">
+        <div class="pre-header container">
+            <div>DC POWER VISA</div>
+            <div>ADDICTIONAL DC SITES <i class="fa-solid fa-chevron-down"></i></div>
+        </div>
+    </section>
+    <section class="header-nav d-flex container align-items-center py-3">
+        <div class="logo">
+            <img class="image-fluid" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo Comics">
+        </div>
+        <nav class="ms-nav">
+            <ul class="d-flex list-unstyled justify-content-between">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP <i class="fa-solid fa-chevron-down"></i></a></li>
+            </ul>
+        </nav>
+        <div class="search-bar position-relative"><input type="text" placeholder="Search">
+            <i class="fa-solid fa-magnifying-glass position-absolute ms-lens"></i>
+        </div>
+    </section>
+
+</header>
