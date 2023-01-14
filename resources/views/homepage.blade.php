22ad8f4116093e6f22e8a94ac8c8aa38e0e2a568
RaimondoCosma <raimondo.cosma@gmail.com>
2023-01-14 18:20:00 +0100

    Aggiunge sezione more-info

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,7 +11,7 @@
 @endsection
 
 @section('page-content')
-    <section class="comics-section">
+    <section class="comics-section pb-3">
         <div class="container position-relative">
             <div class="row">
                 @foreach ($comics as $comic)
@@ -22,6 +22,39 @@
                 @endforeach
             </div>
             <span class="current-series position-absolute text-white py-2 px-3">CURRENT SERIES</span>
+            <div class="text-center mt-5">
+                <button class="btn btn-primary">LOAD MORE</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="more-info">
+        <div class="container d-flex justify-content-between">
+            <div class="cards">
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                        alt="digital comics" /></a>
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="cards">
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}"
+                        alt="dc merchandise" /></a>
+                <span>DC MERCHANDISE</span>
+            </div>
+            <div class="cards">
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}"
+                        alt="subscription" /></a>
+                <span>SUBSCRIPTION</span>
+            </div>
+            <div class="cards">
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}"
+                        alt="comic shop" /></a>
+                <span>COMIC SHOP</span>
+            </div>
+            <div class="cards">
+                <a href="#"><img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}"
+                        alt="dc power visa" /></a>
+                <span>DC POWER VISA</span>
+            </div>
         </div>
     </section>
 @endsection
